61a94f60e61e4bc9be80fea5812a52050bb74cf6
Zhumashova Saadat <saadat9195@gmail.com>
2024-05-24 14:19:58 +0300

    устоновила монгуз и подключила сущности в модуль

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TracksController } from './tracks/tracks.controller';
+import { Artist, ArtistSchema } from './Schemas/Artist.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Album, AlbumSchema } from './Schemas/Album.schema';
+import { Tracks, TracksSchema } from './Schemas/Tracks.schema';
 
 @Module({
-  imports: [],
+  imports: [
+     MongooseModule.forRoot('mongodb://localhost/spotify'),
+     MongooseModule.forFeature([
+       { name: Artist.name, schema: ArtistSchema },
+       { name: Album.name, schema: AlbumSchema },
+       { name: Tracks.name, schema: TracksSchema },
+     ])
+
+  ],
   controllers: [AppController, TracksController],
   providers: [AppService],
 })
