d5ea1cf5b72712dcf687ebd951ee7c24889411c4
Zhumashova Saadat <saadat9195@gmail.com>
2024-05-28 08:40:32 +0300

    #HW-94 Написала cxeму пользователей

---
+++ src/Schemas/User.shema.ts
@@ -0,0 +1,57 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt'
+
+const SALT_WORK_FACTOR = 10;
+export interface UserMethods{
+    generateToken(): void;
+    checkPassword(password: string): Promise<boolean>;
+}
+
+Schema()
+export class User {
+    @Prop({required: true, unique: true})
+    email: string;
+
+    @Prop({required: true})
+    password: string;
+
+    @Prop({required: true})
+    token: string;
+
+    @Prop({
+        required: true,
+        enum: ['admin', 'listener'],
+        default: 'listener'
+    })
+    role: string
+
+    @Prop()
+    displayName: string;
+}
+
+export type UserDocument = User & Document & UserMethods;
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods.generateToken = function (){
+    this.token = crypto.randomUUID();
+};
+
+UserSchema.methods.checkPassword = function (
+    password: string,
+): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.pre('save', async function (){
+    if(!this.isModified('password')) return;
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt)
+});
+
+UserSchema.set('toJSON', {
+    transform: (_, ret) => {
+        delete  ret.password;
+        return ret;
+    },
+});
